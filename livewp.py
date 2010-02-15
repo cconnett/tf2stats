bc4739c8f062ed048cf470ff19bd89e9792d6936
Chris Connett <chris@connett.net>
2010-02-15 00:31:09 -0500

    Initial versions of respawnwave timing module and livewp stats.

---
+++ livewp.py
@@ -0,0 +1,74 @@
+import sqlite3
+import datetime
+import respawnwaves
+
+class AttributedRow(sqlite3.Row):
+    def __getattribute__(self, *args, **kwargs):
+        try:
+            return sqlite3.Row.__getattribute__(self, *args, **kwargs)
+        except AttributeError:
+            return sqlite3.Row.__getitem__(self, *args, **kwargs)
+
+conn = sqlite3.connect('/var/local/chris/pug.db',
+                       detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+conn.row_factory = AttributedRow
+cur = conn.cursor()
+
+deaths_query = """
+select vicplayer, viclife, lives.team as victeam, lives.class as role, time
+from events join lives on events.viclife = lives.id
+where type = 5 and events.round = ?
+order by events.time, events.id"""
+fights_query = """
+select end as "end [timestamp]", winner, midowner, point
+from fights
+where round = ? and point in (4,5) and humiliation = 0
+"""
+nextFight_query = """
+select end, winner, midowner, point
+from fights
+where round = ? and point in (4,5) and humiliation = 0
+and end > ?
+limit 1
+"""
+
+round = 3
+
+cur.execute(deaths_query, (round,))
+deaths = cur.fetchall()
+
+cur.execute(fights_query, (round, ))
+fights = cur.fetchall()
+
+cur.execute('select end from rounds where id = ?', (round,))
+roundEnd = cur.fetchone().end
+
+wc = respawnwaves.WaveCalculator(deaths[0])
+
+unspawnedDeaths = set()
+
+theTime = deaths[0].time
+while theTime < roundEnd:
+    output = False
+    while deaths and deaths[0].time <= theTime:
+        unspawnedDeaths.add(deaths[0])
+        print '%s %s died at %s' % (deaths[0].victeam, deaths[0].role, deaths[0].time)
+        output = True
+        del deaths[0]
+    if fights and fights[0].end <= theTime:
+        print '%s captured spire at %s.' % (fights[0].winner, theTime)
+        output = True
+        wc.notifyOfCapture(fights[0])
+        del fights[0]
+    spawns = [death for death in unspawnedDeaths
+              if wc.timeOfWave(death.victeam, wc.respawnWave(death)) <= theTime]
+    for spawn in spawns:
+        print '%s %s that died at %s respawns at %s' % (spawn.victeam, spawn.role, spawn.time, theTime)
+        output = True
+    unspawnedDeaths -= set(spawns)
+    for death in unspawnedDeaths:
+        if output:
+            print '\t%s %s from %s (up at %s)' % (death.victeam, death.role, death.time,
+                                                  wc.timeOfWave(death.victeam, wc.respawnWave(death)))
+    theTime += datetime.timedelta(seconds=1)
+    if output: print
---
+++ respawnwaves.py
@@ -0,0 +1,66 @@
+import datetime
+
+BIG_WAVE_TIME = 8
+SMALL_WAVE_TIME = 5
+
+class WaveCalculator(object):
+    def __init__(self, firstDeath):
+        self.references = {}
+        self.waveTime = {}
+
+        self.references['Blue'] = self.references['Red'] = (firstDeath.time, 0)
+        self.waveTime['Blue'] = self.waveTime['Red'] = BIG_WAVE_TIME
+
+    def respawnWave(self, death):
+        """Return the respawn wave in which the player killed by the
+        given death event will respawn."""
+
+        victimTeam = death.victeam
+        referenceTime, referenceWave = self.references[victimTeam]
+        timeSinceReference = (death.time - referenceTime)
+        secondsSinceReference = timeSinceReference.seconds + (86400 * timeSinceReference.days)
+
+        wavesSinceReference = (secondsSinceReference - 1) // self.waveTime[victimTeam]
+        respawnWave = referenceWave + wavesSinceReference + 3
+
+        return respawnWave
+
+    def timeOfWave(self, team, wave):
+        """Return the time at which the given respawn wave will occur."""
+
+        referenceTime, referenceWave = self.references[team]
+        respawnTime = (referenceTime +
+                       (wave - referenceWave) *
+                       datetime.timedelta(seconds=self.waveTime[team]))
+        return respawnTime
+
+    def nextWaveOfTime(self, team, time):
+        """Return the wave number of the wave that occurs at or
+        soonest after the given time."""
+
+        referenceTime, referenceWave = self.references[team]
+        nextWave = referenceWave + \
+                   (time - referenceTime).seconds // self.waveTime[team] + \
+                   ((time - referenceTime).seconds % self.waveTime[team] != 0)
+        return nextWave
+
+    def notifyOfCapture(self, fight):
+        """Call this when a capture occurs with the fight that the
+        capture ends, so that this object can change the respawn wave
+        intervals of the teams."""
+
+        nextWave = self.nextWaveOfTime(fight.midowner, fight.end)
+        #print nextWave, self.timeOfWave(fight.midowner, nextWave)
+        self.references[fight.midowner] = (
+            self.timeOfWave(fight.midowner, nextWave), nextWave
+            )
+
+        if fight.point == 4 and fight.midowner == fight.winner:
+            # Midowner capped 4 and their wave time will decrease.
+            self.waveTime[fight.midowner] = SMALL_WAVE_TIME
+        elif fight.point == 5 and fight.midowner != fight.winner:
+            # Midowner lost 4 and their wave time will increase.
+            self.waveTime[fight.midowner]= BIG_WAVE_TIME
+        else:
+            # No other captures change wave times.
+            pass
